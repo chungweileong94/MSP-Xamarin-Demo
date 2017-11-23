da48a67683ae5300749a458ae30c475185a33ca1
Chung Wei Leong <leongchungwei@hotmail.com>
2017-11-23 21:31:04 +0800

    Enable fast renderer for android

--- MSP Xamarin Demo/MSP_Xamarin_Demo.Android/MainActivity.cs
+++ MSP Xamarin Demo/MSP_Xamarin_Demo.Android/MainActivity.cs
@@ -19,6 +19,7 @@ namespace MSP_Xamarin_Demo.Droid
 
             base.OnCreate(bundle);
 
+            global::Xamarin.Forms.Forms.SetFlags("FastRenderers_Experimental");
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
